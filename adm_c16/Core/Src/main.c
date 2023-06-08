6e100d5f3886a4830caba1bc8abd2da02f73d525
amilcar rincon <amilcar.rincon.charris@gmail.com>
2023-06-08 18:28:06 -0400

    se mejoran los ejemplos en asembly

--- adm_c16/Core/Src/main.c
+++ adm_c16/Core/Src/main.c
@@ -23,6 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "asm_func.h"
+#include "ejercicios.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
